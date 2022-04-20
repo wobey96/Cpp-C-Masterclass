6a7e733b2ad0ba05fd47bc522e6d782021221839
Wallace Obey <wobey96@gmail.com>
2022-04-20 17:09:18 -0400

    More on Classes

---
+++ SoloLearn/QueueManagementPart_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std; 
+
+class Queue { 
+	int size; 
+	int* queue; 
+	
+	public:
+	Queue() { 
+		size = 0;
+		queue = new int[100];
+	}
+	void add(int data) { 
+		queue[size] = data; 
+		size++;
+	}
+	void remove() { 
+		if (size == 0) { 
+			cout << "Queue is empty"<<endl; 
+			return; 
+		} 
+		else { 
+			for (int i = 0; i < size - 1; i++) { 
+				queue[i] = queue[i + 1]; 
+			} 
+			size--; 
+		} 
+	} 
+	void print() { 
+		if (size == 0) { 
+			cout << "Queue is empty"<<endl; 
+			return; 
+		} 
+		for (int i = 0; i < size; i++) { 
+			cout<<queue[i]<<" <- ";
+		} 
+		cout << endl;
+	}
+	//your code goes here
+	 friend Queue operator+(Queue& Que1, Queue& Que2)
+	{
+		Queue Que3;
+		for(int i = 0; i < Que1.size; i++)
+		{
+			Que3.add(Que1.queue[i]);
+		}
+		for(int i = 0; i < Que2.size; i++)
+		{
+			Que3.add(Que2.queue[i]);
+		}
+		return Que3;
+	}
+	
+}; 
+
+int main() { 
+	Queue q1; 
+	q1.add(42); q1.add(2); q1.add(8);  q1.add(1);
+	Queue q2;
+	q2.add(3); q2.add(66); q2.add(128);  q2.add(5);
+	Queue q3 = q1+q2;
+	q3.print();
+
+	return 0; 
+} 
