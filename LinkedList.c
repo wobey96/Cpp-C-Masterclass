dcad06fd540951283525a3a8bd436423f56bb42c
wobey000 <wobey96@gmail.com>
2022-07-27 10:14:16 -0600

    Finished Linked List practice

--- LinkedList.c
+++ LinkedList.c
@@ -7,6 +7,88 @@ struct Node
     struct Node* next; 
 };
 
+// 1.) Insert At the front of the linked list
+// 4 step process
+void InsertBegin(struct Node** HeadRef, int newData)
+{
+    printf("Adding %d at beginning of list \n ", newData);
+
+    // new node 
+    struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node)); 
+
+    // assign data to node 
+    NewNode->data = newData; 
+
+    // point to head so we have two thing pointing to head 
+    NewNode->next = (*HeadRef); 
+
+    // move head to point to new node 
+    (*HeadRef) = NewNode; 
+}
+
+// 2.) Insert After a given node
+// 5 step process 
+void InsertMiddle(struct Node* PrevNode, int newData)
+{
+    printf("Inserting %d after %d node \n ", newData, PrevNode->data);
+
+    if(PrevNode == NULL)
+    {
+        printf("We need to insert in the middle\n"); 
+        return; 
+    }
+
+    struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
+
+    NewNode->data = newData; 
+
+    NewNode->next = PrevNode->next; ;
+
+    PrevNode->next = NewNode; 
+}
+
+
+// 3.) Insert At the end of the linked list
+// 6 step process
+void InsertEnd(struct Node** HeadRef, int newData)
+{
+    printf("Inserting %d at the end\n", newData);
+
+    struct Node* NewNode = (struct Node*)malloc(sizeof(struct Node));
+
+    struct Node* LastNode = *HeadRef; 
+
+    NewNode->data = newData; 
+
+    NewNode->next = NULL; 
+
+    if(*HeadRef == NULL)
+    {
+        *HeadRef = NewNode; 
+    }
+
+    while(LastNode->next != NULL)
+    {
+        LastNode = LastNode->next; 
+    }
+
+    LastNode->next = NewNode; 
+}
+
+void traverseList(struct Node** HeadRef)
+{
+    printf("Traversing Function now \n "); 
+    
+    while((*HeadRef) != NULL)
+    {
+        printf(" %d->", (*HeadRef)->data);
+        // printf("Go to nextr head\n");
+        HeadRef = &(*HeadRef)->next; 
+    }
+    printf("\n");
+    printf("\n");
+}
+
 
 int main()
 {
@@ -27,13 +109,21 @@ int main()
     third->data = 3; 
     third->next = NULL; 
 
-    // loop through list
-    struct Node** CurrHead = &head; 
+    struct Node** CurrHead = &head;
 
-    while((*CurrHead) != NULL)
-    {
-        printf("Current Node %d \n", (*CurrHead)->data);
-        printf("Go to nextr head\n");
-        CurrHead = &(*CurrHead)->next; 
-    }
+    // insert new data at beginning 
+    InsertBegin(CurrHead, 5);
+    // loop through list
+    traverseList(CurrHead);
+
+    // insert Node at middle
+    InsertMiddle(second, 10);
+    // loop through list
+    traverseList(CurrHead);
+
+    // Insert at end of list
+    InsertEnd(CurrHead, 20);
+    // loop through list
+    traverseList(CurrHead);
+     
 }
