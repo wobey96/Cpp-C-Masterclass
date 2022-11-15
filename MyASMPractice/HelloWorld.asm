b5dd4631fa7d9e644c97fbfd12343d1d34ecb337
wobey000 <wobey96@gmail.com>
2022-11-15 08:19:43 -0700

    AARCH64 program for practice now back to my OS Development

---
+++ MyASMPractice/HelloWorld.asm
@@ -0,0 +1,22 @@
+#AARCH64 Assembly Hello World 
+
+.global _start
+.section .text
+
+_start:
+    
+    # write system call (system call 64)
+    mov x8, #64 
+    mov x0, #1 
+    ldr x1, =message 
+    mov x2, #13 
+    svc 0 
+
+    # exit system call (system call 95): This is how you exit we need help from the kernel 
+    mov x8, #0x5d
+    mov x0, #0x41
+    svc 0
+
+.section .data
+    message:
+    .ascii "Hello, World\n"
