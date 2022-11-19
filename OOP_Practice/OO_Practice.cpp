003740de19f1dd7e8794dce61fd3b476deb6af86
wobey000 <wobey96@gmail.com>
2022-11-18 23:36:05 -0700

    Reviewing Poly

--- OOP_Practice/OO_Practice.cpp
+++ OOP_Practice/OO_Practice.cpp
@@ -11,17 +11,63 @@
 #include <bitset>
 #include <string.h>
 
-class Fish 
+using namespace std;
+
+class Person 
 {
     private:
-    bool isFreshWaterFish; 
+    int selfAge;; 
 
     public:
-    
+    Person()
+    {
+        cout << "Person Ctor\n"; 
+    }
+    ~Person()
+    {
+        cout << "Person Dtor\n";
+    }
+    int getAge()
+    {
+        return selfAge;
+    }
+    void setAge(int inAge)
+    {
+        selfAge = inAge; 
+    }
+    virtual void MySpeak()
+    {
+        cout << "Hello I'm a Person\n";
+    }
 };
+
+class Student: public Person
+{
+    public:
+    Student()
+    {
+        cout << "Student Ctor\n";
+    }
+    ~Student()
+    {
+        cout << "Student Dtor\n"; 
+    }
+    void MySpeak()
+    {
+        cout << "Hello I'm a Student\n"; 
+    }
+};
+
+void StandAloneSpeak(Person& MyPerson)
+{
+    MyPerson.MySpeak(); 
+}
 
 int main(int argc, char const *argv[])
 {
+    //Person P1; 
+    Student S1; 
+    StandAloneSpeak(S1); 
     
     return 0;
 }
